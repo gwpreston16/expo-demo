eb9611154b22878ef458e02efef20e61f48be9da
Gareth Preston <gareth@gpresto.f9.co.uk>
2021-10-07 19:43:25 +0100

    Updating text and styles for thumbnail

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ export default function App() {
         <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
           <Text style={styles.buttonText}>Share this photo</Text>
         </TouchableOpacity>
-        </View>
+      </View>
     );
   }
 
@@ -66,7 +66,7 @@ export default function App() {
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
       <Text style={styles.instructions}>
-        Open up App.js to start working on your app! test
+        To share a photo from your phone with a friend, just press the button below!
       </Text>
       <TouchableOpacity
         onPress={openImagePickerAsync}
@@ -99,10 +99,17 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     padding: 20,
     borderRadius: 5,
+    marginVertical: 15
   },
   buttonText: {
     fontSize: 20,
     fontFamily: 'Montserrat',
     color: '#fff',
-  }, 
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    marginVertical: 15
+  },
 });
